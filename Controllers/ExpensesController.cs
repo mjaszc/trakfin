47155317a6e2f8b90eb6fdf81521983b531eecf5
jasmar2 <jaszczaninmarceli@gmail.com>
2024-06-24 15:23:20 +0200

    Add project files.

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Trakfin.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        // 
+        // GET: /Expenses/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // TEST ROUTE
+        // GET: /Expenses/Welcome/ 
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
