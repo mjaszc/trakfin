ecbb71ff21c36d85117ada779d1ec843247168e7
jasmar2 <jaszczaninmarceli@gmail.com>
2024-09-18 21:02:58 +0200

    Added app services and mssql db to the infra part

--- infra/main.tf
+++ infra/main.tf
@@ -8,6 +8,9 @@ terraform {
 }
 
 variable "subscription_id" {}
+variable "mssql_admin_login" {}
+variable "mssql_admin_password" {}
+variable "mssql_connection_string" {}
 variable "publisher_email" {}
 variable "publisher_name" {}
 
@@ -26,7 +29,7 @@ resource "azurerm_container_registry" "acr_api" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
-  admin_enabled       = false
+  admin_enabled       = true
 
   provisioner "local-exec" {
     command = "sh ../scripts/push_api_container_to_acr.sh"
@@ -38,10 +41,79 @@ resource "azurerm_container_registry" "acr_web" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
-  admin_enabled       = false
+  admin_enabled       = true
 
   provisioner "local-exec" {
     command = "sh ../scripts/push_web_container_to_acr.sh"
+  }
+}
+
+resource "azurerm_user_assigned_identity" "assigned_id" {
+  name                = "jasmar2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_mssql_server" "trakfin" {
+  name                         = "trakfinserver"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = "polandcentral"
+  version                      = "12.0"
+  administrator_login          = var.mssql_admin_login
+  administrator_login_password = var.mssql_admin_password
+  minimum_tls_version          = "1.2"
+}
+
+resource "azurerm_mssql_firewall_rule" "firewall_rule" {
+  name             = "TrakfinFirewallRule1"
+  server_id        = azurerm_mssql_server.trakfin.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name                 = "TrakfinDB"
+  server_id            = azurerm_mssql_server.trakfin.id
+  collation            = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb          = 2
+  sku_name             = "Basic"
+  zone_redundant       = false
+  storage_account_type = "Local"
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+# Create a key vault with access policies which allow for the current user to get, list, create, delete, update, recover, purge and getRotationPolicy for the key vault key and also add a key vault access policy for the Microsoft Sql Server instance User Managed Identity to get, wrap, and unwrap key(s)
+resource "azurerm_key_vault" "kv" {
+  name                       = "TrakfinKV"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Create",
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover",
+      "List"
+    ]
   }
 }
 
@@ -54,7 +126,7 @@ resource "azurerm_api_management" "api" {
   sku_name            = "Developer_1"
 }
 
-resource "azurerm_api_management_api" "example" {
+resource "azurerm_api_management_api" "api_management" {
   name                = "TrakfinAPI"
   resource_group_name = azurerm_resource_group.rg.name
   api_management_name = azurerm_api_management.api.name
@@ -68,3 +140,59 @@ resource "azurerm_api_management_api" "example" {
   }
 }
 
+resource "azurerm_service_plan" "sp" {
+  name                = "TrakfinServicePlan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "api_app" {
+  name                = "TrakfinAPI"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.sp.location
+  service_plan_id     = azurerm_service_plan.sp.id
+
+  site_config {
+    always_on = false
+    # Link the api management straight to the api
+    api_management_api_id = azurerm_api_management_api.api_management.id
+
+    application_stack {
+      docker_image_name        = "trakfinapi:latest"
+      docker_registry_url      = "https://${azurerm_container_registry.acr_api.login_server}"
+      docker_registry_username = azurerm_container_registry.acr_api.admin_username
+      docker_registry_password = azurerm_container_registry.acr_api.admin_password
+    }
+  }
+
+  app_settings = {
+    "TrakfinContext" = var.mssql_connection_string
+  }
+}
+
+resource "azurerm_linux_web_app" "web_app" {
+  name                = "TrakfinWeb"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.sp.location
+  service_plan_id     = azurerm_service_plan.sp.id
+
+  site_config {
+    always_on = false
+
+    application_stack {
+      docker_image_name        = "trakfinweb:latest"
+      docker_registry_url      = "https://${azurerm_container_registry.acr_web.login_server}"
+      docker_registry_username = azurerm_container_registry.acr_web.admin_username
+      docker_registry_password = azurerm_container_registry.acr_web.admin_password
+    }
+  }
+
+  app_settings = {
+    "API_URL" = var.api_url
+    "TrakfinContext" = var.mssql_connection_string
+  }
+
+  # Retrieve TrakfinContext variable from secrets
+}
