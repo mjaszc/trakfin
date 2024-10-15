f4c1d0fd65944c080cc54553e467d6569fc01707
Marceli <jaszczaninmarceli@gmail.com>
2024-10-15 11:25:43 +0200

    Added message handler

---
+++ Trakfin/Middlewares/CancelledTaskMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+namespace Trakfin.Middlewares
+{
+    // create handler
+    class CancelledTaskBugWorkaroundMessageHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+                // Try to suppress response content when the cancellation token has fired; ASP.NET will log to the Application event log if there's content in this case.
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return new HttpResponseMessage(HttpStatusCode.OK);
+                }
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                if (IsAspNetBugException(ex))
+                    return new HttpResponseMessage(HttpStatusCode.OK);
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+
+
+        }
+        private static bool IsAspNetBugException(Exception exception)
+        {
+            return
+                (exception is TaskCanceledException || exception is OperationCanceledException)
+                &&
+                exception.StackTrace.Contains("System.Web.HttpApplication.ExecuteStep");
+        }
+    }
--- Trakfin/Program.cs
+++ Trakfin/Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using Trakfin.Data;
 using Microsoft.AspNetCore.Identity;
+using Trakfin.Middlewares;
+using System.Web.Http;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        HttpConfiguration config = new HttpConfiguration();
 
         builder.Services.AddDbContext<TrakfinContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("TrakfinContext"), sqlServerOptionsAction: sql_opt =>
@@ -39,12 +42,15 @@ public class Program
 
         app.UseAuthorization();
 
+        config.MessageHandlers.Add(new CancelledTaskBugWorkaroundMessageHandler());
+
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
 
         app.MapRazorPages();
         app.Run();
+
     }
 }
 
